14b3c70f29d62d1470451eea0cfedb6a1bd30f5e
Oscar Ciudad <oscar@jacho.net>
2016-05-03 19:27:01 +0200

    Set to production/stable

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description=open("README.rst").read(),
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Framework :: Sphinx :: Extension',
