b34b44c6fa96aa9ffde2efc07859d441e99c32ab
Stein Heselmans <teh@melexis.com>
2017-05-24 09:46:04 +0200

    Version 1.3.0: complete packaging on github with travis-ci and pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 project_url = 'https://github.com/SteinHeselmans/sphinx-traceability-extension'
-version = '1.2.6'
+version = '1.3.0'
 
 requires = ['Sphinx>=0.6', 'docutils']
 
