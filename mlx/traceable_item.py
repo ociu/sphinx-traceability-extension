1113e352f253a873b5b3febedaa8366c3e238fc6
Stein Heselmans <teh@melexis.com>
2017-11-09 09:55:54 +0100

    Finish adding/moving/getting relations in TraceableItem-class

--- mlx/traceable_item.py
+++ mlx/traceable_item.py
@@ -30,26 +30,71 @@ class TraceableItemCollection(dict):
             return self.relations[forward]
         return None
 
+    def add_item(self, item):
+        '''
+        Add a TraceableItem to the list
+
+        Args:
+            item (TraceableItem): Traceable item to add
+        '''
+        self.items[item.get_id()] = item;
+
+    def purge(self, docname):
+        '''
+        Purge any item from the list which matches the given docname
+
+        Args:
+            docname (str): Name of the document to purge for
+        '''
+        for item in self.items:
+            if item.get_doc_name() == docname:
+                del self.items[item.get_id()]
+
+    def add_relation(self, sourceid, relation, targetid):
+        '''
+        Add relation between two items
+
+        The function adds the forward and the automatic reverse relation.
+
+        Args:
+            sourceid (str): ID of the source item
+            relation (str): Relation between source and target item
+            targetid (str): ID of the target item
+        '''
+        # Fail if relation is unknown
+        if relation not in self.relations:
+            raise ValueError('Relation {name} not known'.format(name=relation))
+        # Fail if source item is unknown
+        if sourceid not in self.items:
+            raise ValueError('Item {name} not known'.format(name=sourceid))
+        # Add forward relation
+        self.items[sourceid].add_relation(relation, targetid)
+        # Add placeholder if target item is unknown
+        if targetid not in self.items:
+            tgt = TraceableItem(targetid, True)
+            self.add_item(tgt)
+        # When reverse relation exists, add it to target-item
+        reverse_relation = self.get_reverse_relation(relation)
+        if not reverse_relation:
+            self.items[targetid].add_relation(reverse_relation, sourceid)
+
 
 class TraceableItem(object):
     '''
     Storage for a traceable documentation item
     '''
 
-    def __init__(self, itemid, itemlib=None, placeholder=False):
+    def __init__(self, itemid, placeholder=False):
         '''
         Initialize a new traceable item
 
         Args:
             itemid (str): Item identification
-            itemlib (dict): Library of items in which this traceable item is contained
             placeholder (bool): Internal use only
         '''
         self.id = itemid
-        self.itemlib = itemlib
-        if self.itemlib is not None:
-            self.itemlib[itemid] = self
-        self.relations = {}
+        self.explicit_relations = {}
+        self.implicit_relations = {}
 
     def get_id(self):
         '''
@@ -64,22 +109,81 @@ class TraceableItem(object):
         self.docname = docname
         self.lineno = lineno
 
-    def add_relation(self, relation, target):
+    def _add_relation(self, database, relation, target):
         '''
         Add a relation to another traceable item
 
         Args:
             relation (str): Name of the relation
             target (str): Item identification of the targetted traceable item
+            database (dict): Dictionary to add the relation to
         '''
-        if relation not in self.relations:
-            self.relations[relation] = []
-        # Add forward relation
-        if target not in self.relations[relation]:
-            self.relations[relation].append(target)
-        # Add reverse relation
-        if self.itemlib:
-            if target in self.itemlib:
-                reverse = self.itemlib.get_reverse_relation(relation)
-            #self.itemlib[target].add_relation(reverse,
+        if relation not in database:
+            database[relation] = []
+        if target not in database[relation]:
+            database[relation].append(target)
 
+    def _remove_relation(self, database, relation, target):
+        '''
+        Delete a relation to another traceable item
+
+        Args:
+            relation (str): Name of the relation
+            target (str): Item identification of the targetted traceable item
+            database (dict): Dictionary to remove the relation from
+        '''
+        if relation in database:
+            if target in database[relation]:
+                database[relation].remove(target)
+
+    def add_relation(self, relation, target, implicit=False):
+        '''
+        Add a relation to another traceable item
+
+        Note: using this API, the automatic reverse relation is not created. Adding the relation
+        through the TraceableItemCollection class performs the adding of automatic reverse
+        relations.
+
+        Args:
+            relation (str): Name of the relation
+            target (str): Item identification of the targetted traceable item
+            implicit (bool): If true, an explicitely expressed relation is added here. If false, an implicite
+                             (e.g. automatic reverse) relation is added here.
+        '''
+        # When relation is already explicit, we shouldn't add. When relation-to-add is explicit, it is an error.
+        if self.get_relations(relation, explicit=True, implicit=False):
+            if implicit == False:
+                print('Error duplicating {src} {rel} {tgt}'.format(src=self.get_id(), rel=relation, tgt=target))
+        # When relation is already implicit, we shouldn't add. When relation-to-add is explicit, it should move
+        # from implicit to explicit.
+        elif self.get_relations(relation, explicit=False, implicit=True):
+            if implicit == False:
+                print('Warning duplicating {src} {rel} {tgt}, moving to explicit'.format(src=self.get_id(), rel=relation, tgt=target))
+                self._remove_relation(self.implicit_relations, relation, target)
+                self._add_relation(self.explicit_relations, relation, target)
+        # Otherwise it is a new relation, and we add to the selected database
+        else:
+            if implicit == False:
+                database = self.explicit_relations
+            else:
+                database = self.implicit_relations
+            self._add_relation(database, relation, target)
+
+    def get_relations(self, relation, explicit=True, implicit=True):
+        '''
+        Get a sorted list of relations to other traceable item(s)
+
+        Args:
+            relation (str): Name of the relation
+            explicit (bool): If true, explicitely expressed relations are included in the returned list.
+            implicit (bool): If true, implicitely expressed relations are included in the returned list.
+        '''
+        relations = []
+        if explicit == True:
+            if relation in self.explicit_relations.keys():
+                relations.extend(self.explicit_relations[relation])
+        if implicit == True:
+            if relation in self.implicit_relations.keys():
+                relations.extend(self.implicit_relations[relation])
+        relations.sort()
+        return relations
--- tests/test_traceable_item.py
+++ tests/test_traceable_item.py
@@ -4,16 +4,93 @@ import mlx.traceable_item as dut
 
 
 class TestTraceableItem(TestCase):
-    name = 'some-random$name\'with<\"weird@symbols'
+    identification = 'some-random$name\'with<\"weird@symbols'
+    fwd_relation = 'some-random-forward-relation'
+    identification_tgt = 'another-item-to-target'
 
     def test_init(self):
-        item = dut.TraceableItem(self.name)
-        self.assertEqual(self.name, item.get_id())
+        item = dut.TraceableItem(self.identification)
+        self.assertEqual(self.identification, item.get_id())
 
-    def test_init_lib(self):
-        lib = dut.TraceableItemCollection()
-        item = dut.TraceableItem(self.name, lib)
-        self.assertEqual(self.name, item.get_id())
-        print(lib)
-        self.assertEqual(self.name, lib[self.name].get_id())
+    def test_add_get_relation_explicit(self):
+        item = dut.TraceableItem(self.identification)
+        # Initially no relations (explicit+implicit)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(0, len(out))
+        # Add an explicit relation
+        item.add_relation(self.fwd_relation, self.identification_tgt)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        # Add the same explicit relation, should not change (no duplicates)
+        item.add_relation(self.fwd_relation, self.identification_tgt)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        # Add the same implicit relation, should not change (is already explicit)
+        item.add_relation(self.fwd_relation, self.identification_tgt, implicit=True)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+
+    def test_add_get_relation_implicit(self):
+        item = dut.TraceableItem(self.identification)
+        # Initially no relations (explicit+implicit)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(0, len(out))
+        # Add an implicit relation
+        item.add_relation(self.fwd_relation, self.identification_tgt, implicit=True)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(0, len(out))
+        # Add the same implicit relation, should not change (no duplicates)
+        item.add_relation(self.fwd_relation, self.identification_tgt, implicit=True)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(0, len(out))
+        # Add the same explicit relation, should move the relation to be explicit
+        item.add_relation(self.fwd_relation, self.identification_tgt)
+        out = item.get_relations(self.fwd_relation)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+        out = item.get_relations(self.fwd_relation, explicit=False)
+        self.assertEqual(0, len(out))
+        out = item.get_relations(self.fwd_relation, implicit=False)
+        self.assertEqual(1, len(out))
+        self.assertEqual(out[0], self.identification_tgt)
+
+
 
