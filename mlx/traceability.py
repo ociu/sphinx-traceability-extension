dff9b88c1f05ddafea66fdd2f8a1f2091ae74a00
Stein Heselmans <teh@melexis.com>
2018-01-30 11:26:14 +0100

    Use links to raw bower js/css

--- mlx/traceability.py
+++ mlx/traceability.py
@@ -925,8 +925,8 @@ def setup(app):
 
     # Javascript and stylesheet for the tree-view
     # app.add_javascript('jquery.js') #note: can only be included once
-    app.add_javascript('//raw.githubusercontent.com/aexmachina/jquery-bonsai/master/jquery.bonsai.js')
-    app.add_stylesheet('//raw.githubusercontent.com/aexmachina/jquery-bonsai/master/jquery.bonsai.css')
+    app.add_javascript('https://cdn.rawgit.com/aexmachina/jquery-bonsai/master/jquery.bonsai.js')
+    app.add_stylesheet('https://cdn.rawgit.com/aexmachina/jquery-bonsai/master/jquery.bonsai.css')
     app.add_javascript('traceability.js')
 
     # Configuration for adapting items through a callback
