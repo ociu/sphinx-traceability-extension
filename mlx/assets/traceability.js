523e022699c23e8f6bf1229377fbec003d287690
Jasper Craeghs <jce@melexis.com>
2019-07-23 18:00:07 +0200

    Replace 'Hide links' with arrowhead icon on righthand side in the same color as the links

--- mlx/assets/traceability.js
+++ mlx/assets/traceability.js
@@ -1,31 +1,41 @@
+// item-tree
 jQuery(function () {
     $('ul.bonsai').bonsai();
 });
 
+// item (traceability_collapsible_links == True)
 jQuery.fn.extend({
-    prependHideLinks: function () {
-        var relations = $(this)
-        var hideLinks = $('<a>', {
-            text: 'Hide links',
-            click: function () {
-                if (relations.is(':visible')) {
-                    relations.hide();
-                    $(this).text('Show links');
-                } else {
-                    relations.show();
-                    $(this).text('Hide links');
-                }
-            }
-        });
+    prependHideLinks: function (admonition) {
+        var relations = $(this);
+
         if (relations.children().length > 0) {
-            console.log(relations.children().length);
-            $(this).before(hideLinks);
+            var arrow = $('<i>', {
+                class: "fa fa-angle-up",
+                click: function () {
+                    relations.toggle();
+                    $(this).toggleClass("fa-angle-up fa-angle-down");
+                }
+            });
+
+            var linkColor = relations.children('dt').first().css('color');
+            console.log(linkColor);
+            var arrowDiv = $('<div>', {
+                css: {
+                    'display': 'inline-block',
+                    'float': 'right',
+                    'font-size': '150%',
+                    'color': linkColor,
+                    'padding-right': '1rem'
+                },
+                append: arrow
+            });
+            admonition.after(arrowDiv);
         }
     }
 });
 
 $(document).ready(function () {
-    $('div.toggle_links div.admonition:first-child').each(function (i) {
-        $(this).siblings('dl.simple').prependHideLinks();
+    $('div.collapsible_links div.admonition:first-child').each(function (i) {
+        $(this).siblings('dl.simple').prependHideLinks($(this));
     });
 });
