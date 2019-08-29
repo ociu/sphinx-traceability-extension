4ddb0553b5cda0f97d256c6e965290437f3abcd3
Jasper Craeghs <jce@melexis.com>
2019-08-29 13:30:29 +0200

    Drop py3.4 and sphinx<2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 project_url = 'https://github.com/melexis/sphinx-traceability-extension'
 
-requires = ['Sphinx>=0.6', 'docutils', 'natsort', 'matplotlib', 'python-decouple', 'requests']
+requires = ['Sphinx>=2.1', 'docutils', 'natsort', 'matplotlib', 'python-decouple', 'requests']
 
 setup(
     name='mlx.traceability',
@@ -28,7 +28,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
