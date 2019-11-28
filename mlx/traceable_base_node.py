78bc6d3d5f15fdaee574e00474865f8432d800ea
Jasper Craeghs <jce@melexis.com>
2019-11-28 16:01:35 +0100

    Refactor caption part of make_internal_item_ref

--- mlx/traceable_base_node.py
+++ mlx/traceable_base_node.py
@@ -52,7 +52,7 @@ class TraceableBaseNode(nodes.General, nodes.Element, ABC):
             collection (TraceableCollection): Collection for which to generate the nodes.
         """
 
-    def make_internal_item_ref(self, app, item_id, caption=True):
+    def make_internal_item_ref(self, app, item_id, show_caption=True):
         """
         Creates a reference node for an item, embedded in a
         paragraph. Reference text adds also a caption if it exists.
@@ -61,18 +61,18 @@ class TraceableBaseNode(nodes.General, nodes.Element, ABC):
         item_info = env.traceability_collection.get_item(item_id)
 
         p_node = nodes.paragraph()
-        caption_on_hover = None
 
         # Only create link when target item exists, warn otherwise (in html and terminal)
         if self.has_warned_about_undefined(item_info):
             txt = nodes.Text('%s not defined, broken link' % item_id)
             p_node.append(txt)
         else:
-            if item_info.caption and caption:
-                caption = ' : {}'.format(item_info.caption)
-            else:
-                caption = ''
-                if item_info.caption:
+            caption_on_hover = None
+            caption = ''
+            if item_info.caption:
+                if show_caption:
+                    caption = ' : {}'.format(item_info.caption)
+                else:
                     caption_on_hover = nodes.inline('', item_info.caption)
                     caption_on_hover['classes'].append('popup_caption')
 
