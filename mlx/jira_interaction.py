861174ad0d7cd2f458d32df57468ed71037e9ae1
Jasper Craeghs <jasper.craeghs@gmail.com>
2020-08-09 13:32:59 +0200

    Use caption as description of ticket if item has no body

--- mlx/jira_interaction.py
+++ mlx/jira_interaction.py
@@ -84,7 +84,10 @@ def create_unique_issues(item_ids, jira, general_fields, settings, traceability_
 
         fields['project'] = project_id_or_key
         fields[jira_field_id] = jira_field
-        fields['description'] = item.get_content()
+        content = item.get_content()
+        if not content:
+            content = item.caption
+        fields['description'] = content
         if assignee:
             fields['assignee'] = {'name': item.get_attribute('assignee')}
 
